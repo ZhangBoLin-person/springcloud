50cf67021dc884a1c2aa3830a07a4e5a2fc1213b
augenstern <286322812@qq.com>
2020-10-26 21:21:16 +0800

    1.spring-cloud的demo(无springboot) 2.更新消费者请求路径及新增消费者启动器 3.repeat 4.新增eureka模块，通过调用7001接口访问eureka页面

---
+++ springcloud-eurake/src/main/java/com/sanyue/springcloud/EurekaServer_7001.java
@@ -0,0 +1,19 @@
+package com.sanyue.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author BoLin
+ * @create 2020-10-26 21:12
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServer_7001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer_7001.class,args);
+    }
+
+}
