a2698e3da978439d01041b22e6a4776e98013b1c
augenstern <286322812@qq.com>
2020-10-26 17:19:27 +0800

    1.spring-cloud的demo(无springboot) 2.更新消费者请求路径及新增消费者启动器

---
+++ springcloud-consumer-dept/src/main/java/com/sanyue/springcloud/DeptConsumer_80.java
@@ -0,0 +1,16 @@
+package com.sanyue.springcloud;
+
+import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author BoLin
+ * @create 2020-10-26 16:56
+ */
+@SpringBootApplication
+public class DeptConsumer_80 {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConsumer_80.class,args);
+    }
+}
--- springcloud-consumer-dept/src/main/java/com/sanyue/springcloud/controller/DeptConsumerController.java
+++ springcloud-consumer-dept/src/main/java/com/sanyue/springcloud/controller/DeptConsumerController.java
@@ -1,17 +1,21 @@
 package com.sanyue.springcloud.controller;
 
 import com.sanyue.springcloud.pojo.Dept;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @author BoLin
  * @create 2020-10-26 14:06
  */
-@Controller
+@RestController
 public class DeptConsumerController {
 
     @Autowired
@@ -19,12 +23,21 @@ public class DeptConsumerController {
 
     private static final String REST_URL_PREFIX = "http://localhost:8081";
 
-//    http://localhost:8081/list
+    //http://localhost:8081/get/1
     @RequestMapping("/consumer/dept/get/{id}")
     public Dept getDept(@PathVariable("id") Long id){
-//        return restTemplate.getForObject();
-        return null;
+        return restTemplate.getForObject(REST_URL_PREFIX+"/get/"+id,Dept.class);
     }
 
+    @RequestMapping("/consumer/dept/add")
+    public boolean add(Dept dept){
+        return restTemplate.postForObject(REST_URL_PREFIX+"/add",dept,Boolean.class);
+    }
+
+    //http://localhost:8081/list
+    @RequestMapping("/consumer/dept/list")
+    public List<Dept> getAll(){
+        return restTemplate.getForObject(REST_URL_PREFIX+"/list",List.class);
+    }
 
 }
