bbf91a3e5d078d8201fa0d827bfa2ccfd786ebc7
augenstern <286322812@qq.com>
2020-11-11 09:26:45 +0800

    'zuul--test'

---
+++ springcloud-zuul-9527/src/main/java/com/sanyue/springcloud/ZuulApplication_9527.java
@@ -0,0 +1,17 @@
+package com.sanyue.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author BoLin
+ * @create 2020-11-10 17:49
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication_9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication_9527.class,args);
+    }
+}
